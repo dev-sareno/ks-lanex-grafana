dd3842fa4c42ca0546211b1e039372169d0278c6
dev-sareno <dev.sareno@gmail.com>
2023-07-11 19:48:23 +0800

    chore: Implement python app custom metrics

--- pyapp/app.py
+++ pyapp/app.py
@@ -1,15 +1,25 @@
-import random
 from flask import Flask
-import time
+from prometheus_client import start_http_server
+import manager
+
 
 app = Flask(__name__)
 
+
+def get_base():
+    return "Hello, World!"
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return manager.get_hello()
+
 
 @app.route("/api/slow")
 def slow():
-    v = random.randint(1, 5)
-    time.sleep(v)
-    return f"I slept for {v} seconds!"
+    return manager.get_slow()
+
+
+if __name__ == "__main__":
+    # Start up the server to expose the metrics.
+    start_http_server(9090)
+    app.run(debug=False, host="0.0.0.0", port=8001)
