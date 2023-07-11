597f3876c57f882247e1cade200ec657791d04e2
dev-sareno <dev.sareno@gmail.com>
2023-07-11 19:18:36 +0800

    chore: Add /api/slow route

--- pyapp/app.py
+++ pyapp/app.py
@@ -1,7 +1,15 @@
+import random
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/api/slow")
+def slow():
+    v = random.randint(1, 5)
+    time.sleep(v)
+    return f"I slept for {v} seconds!"
